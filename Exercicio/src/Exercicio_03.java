0d9e7548e7ed994a88282f01813621b510c83fad
Douglas de lima <skylyfe1234@gmail.com>
2025-05-29 21:53:57 -0300

    Exercicio3 diferença do produto

---
+++ Exercicio/src/Exercicio_03.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercicio_03 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int A, B, C, D;
+		int DiferencaProduto;
+		
+		System.out.print("Digite o 1° valor: ");
+		A = scan.nextInt();
+		System.out.print("Digite o 2° valor: ");
+		B = scan.nextInt();
+		System.out.print("Digite o 3° valor: ");
+		C = scan.nextInt();
+		System.out.print("Digite o 4° valor: ");
+		D = scan.nextInt();
+		
+		DiferencaProduto = (A*B - C*D);
+		
+		System.out.println("DIFERENÇA = "+ DiferencaProduto);
+	}
+
+}
